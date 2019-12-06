07a677422909c4bd3ff875c27f2d657b2e8c3242
thiagology <46846692+thiagology@users.noreply.github.com>
2019-12-06 17:12:29 -0300

    Tela inicial
    primeira ideia de tela inicial

---
+++ AlgorEats/src/algoreats/AlgorEatsApp.java
@@ -0,0 +1,26 @@
+package algoreats;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class AlgorEatsApp extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("Algor Eats - Delivery Recife");
+		Pane rootScene = FXMLLoader.load(getClass().getResource("gui/TelaInicial.fxml"));
+		
+		Scene scene = new Scene(rootScene, 512, 602);
+		primaryStage.setScene(scene);
+		primaryStage.show(); 
+		
+	}
+	
+	public static void main(String[] args) {
+		AlgorEatsApp.launch(args);
+	}
+
+}
---
+++ AlgorEats/src/algoreats/gui/TelaInicialController.java
@@ -0,0 +1,48 @@
+package algoreats.gui;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class TelaInicialController {
+
+    @FXML private ImageView imgHead;
+    @FXML private ImageView imgFast;
+    @FXML private ImageView imgMassas;
+    @FXML private ImageView imgMar;
+    @FXML private ImageView imgSalada;
+    @FXML private ImageView imgSobremesa;
+    @FXML private Button btFast;
+    @FXML private Button btMassa;
+    @FXML private Button btMar;
+    @FXML private Button btSalada;
+    @FXML private Button btSobremesa;
+    
+    
+    
+    @FXML
+    void onClickFast(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickMar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickMassa(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSalada(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickSobremesa(ActionEvent event) {
+
+    }
+
+}
