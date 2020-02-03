2a33f874bc5a12d20b706a1b6008fee7a09065fe
thiagology <46846692+thiagology@users.noreply.github.com>
2020-02-03 19:51:58 -0300

    repositorios

--- AlgorEats/src/algoreats/dados/RepositorioCliente.java
+++ AlgorEats/src/algoreats/dados/RepositorioCliente.java
@@ -1,5 +1,58 @@
 package algoreats.dados;
 
-public class RepositorioCliente {
+import java.util.ArrayList;
 
+import algoreats.negocio.beans.Endereco;
+
+public class RepositorioCliente {
+	private ArrayList <Endereco> enderecos;
+	private static RepositorioCliente instance;
+	
+	public static RepositorioCliente getInstance() {
+		if(instance == null) {
+			instance = new RepositorioCliente();
+		}
+		return instance;
+	}
+	
+	public void criarCliente(Endereco e) {
+		if(clienteExiste(e.getNumero()) == false) {
+			this.enderecos.add(e);
+		}
+		else {
+			//add exception
+		}
+	}
+
+	public void excluirCliente(Endereco e) {
+		if(clienteExiste(e.getNumero()) == true) {
+			this.enderecos.remove(e);
+		}
+		else {
+			//add exception
+		}
+	}
+	
+	public Endereco buscarCliente(int numero) {
+		for(Endereco e : enderecos) {
+			if(e.getNumero() == numero) {
+				return e;
+			}
+			else{
+				//add exception
+			}
+		}
+		return null;
+	}
+	
+	public void alterarCliente(Endereco e, Endereco novo) {
+		if(clienteExiste(e.getNumero()) == true) {
+			int i = this.enderecos.indexOf(e);
+			this.enderecos.set(i, novo);
+		}
+	}
+	
+	public boolean clienteExiste(int numero) {
+		return buscarCliente(numero) != null;
+	}
 }
--- AlgorEats/src/algoreats/dados/RepositorioPedido.java
+++ AlgorEats/src/algoreats/dados/RepositorioPedido.java
@@ -16,5 +16,49 @@ public class RepositorioPedido {
 		return instance;
 	}
 	
+	public void criarPedido(Pedido p) {
+		if(pedidoExiste(p.getNome()) == false) {
+			this.pedidos.add(p);
+		}
+		else {
+			//add exception
+		}
+	}
+	
+	public Pedido buscarPedido(String nome) {
+		for(Pedido p : pedidos) {
+			if(p.getNome() == nome) {
+				return p;
+			}
+			else {
+				return null; //add exception
+			}
+		}
+		return null;
+	}
+
+	public boolean pedidoExiste(String nome) {
+		return buscarPedido(nome) != null;
+	}
+	
+	public void excluirPedido(Pedido p) {
+		if(pedidoExiste(p.getNome()) == true) {
+			this.pedidos.remove(p);
+		}		
+		else {
+			//add exception
+		}
+	}
+	
+	public void alterarPedido(Pedido p) {
+		if(buscarPedido(p.getNome()) != null) {
+			int i = this.pedidos.indexOf(p);
+			this.pedidos.set(i, p);
+		}
+		else {
+			//add exception
+		}
+	}
+	
 	
 }
